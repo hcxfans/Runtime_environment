a22fdded87982743643d2a6df188486bc94c0e79
Ms Huang <115678869+hcxfans@users.noreply.github.com>
2025-03-19 21:14:55 +0800

    Add files via upload

---
+++ ltp-hanlp-inst.bat
@@ -0,0 +1,7 @@
+pip install pycantonese
+pip install pyltp-0.2.1-cp37-cp37m-win_amd64.whl
+pip install graphviz
+pip install zhconv
+pip install PyPDF2
+pip install hanlp==2.1.0
+pip install hanlp[full] -U
